e13ffbbcc2acd5687006c878150141e4492e2075
sushant1305 <sushant.mad@gmail.com>
2021-03-23 22:38:56 +0530

    Initial Commit

--- work_1.py
+++ work_1.py
@@ -1,1 +1,9 @@
-print("hello world")
+import itertools
+
+if __name__ == '__main__':
+    S = int(input())
+    while S < 10**4:
+        S = str(S)
+        a_iter = itertools.groupby(S, lambda x : x[0])
+        print (a_iter)
+        break
